20070846e5f621d396cff7671dce87179fd2f510
evgenyr <evgenyr@sapling-inc.com>
2017-10-23 12:52:42 -0400

    tcpServer.py and tcpClient are added

--- Networking/tcpServer.py
+++ Networking/tcpServer.py
@@ -9,5 +9,19 @@ def Main():
 
     s.listen(1)
     c, addr = s.accept()
+    print("Connection from client: " + str(addr))
+    while True:
+        data = c.recv(1024)
+        if not data:
+            break
+        print("From connected user: " + data)
+        data = data.upper()
+        print("Sending: " + data)
+        c.send(data)
+    c.close()
 
-    
+if __name__ =='__main__':
+    Main()
+
+
+
